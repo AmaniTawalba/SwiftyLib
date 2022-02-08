63e92553fce53b180d2becb7fd2e1775a5b63203
Amani Tawalbeh <atawalbeh@blessedtreeit.com>
2022-02-08 16:23:53 +0200

    first commit

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Amani Tawalbeh on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
