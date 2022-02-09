bbbfcdb93727ffca2846b0c576250b84a155d3be
Amani Tawalbeh <atawalbeh@blessedtreeit.com>
2022-02-09 09:14:52 +0200

    fixes

--- SwiftyLib/SwiftyLib.swift
+++ SwiftyLib/SwiftyLib.swift
@@ -5,4 +5,17 @@
 //  Created by Amani Tawalbeh on 2/8/22.
 //
 
-import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
